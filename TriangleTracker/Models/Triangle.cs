840ecdf908356e6096e0e210f22660d6f4290152
Shanen Cross <shanencross@yahoo.com>
2021-04-14 20:44:09 -0700

    Pass first three Triangle tests
    Co-authored-by: Jesse Callahan <jessetylercallahan@gmail.com>

---
+++ TriangleTracker/Models/Triangle.cs
@@ -0,0 +1,13 @@
+namespace TriangleTracker.Models {
+  public class Triangle {
+    public int Side1 { get; set; }
+    public int Side2 { get; set; }
+    public int Side3 { get; set; }
+
+    public Triangle(int side1, int side2, int side3) {
+      Side1 = side1;
+      Side2 = side2;
+      Side3 = side3;
+    }
+  }
+}
