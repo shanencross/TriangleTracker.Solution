d1b7d3da761bd9fdbbf8ad550629e3d9cc3e8fd7
Shanen Cross <shanencross@yahoo.com>
2021-04-14 19:48:48 -0700

    Rename tests folder and add test script outline
    Co-authored-by: Jesse Callahan <jessetylercallahan@gmail.com>

---
+++ TriangleTracker.Tests/TriangleTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleTracker;
+
+namespace TriangleTracker.Tests {
+  [TestClass]
+  public class TriangleTrackerTests {
+    // [TestMethod]
+    // public void TriangleTest() {
+
+    // }
+  }
+}
