18340b7159eaedb1d412800f80a29ffbd9846b90
Shanen Cross <shanencross@yahoo.com>
2021-04-14 20:42:31 -0700

    Wrote first three Triangle tests
    Co-authored-by: Jesse Callahan <jessetylercallahan@gmail.com>

--- TriangleTracker.Tests/TriangleTests.cs
+++ TriangleTracker.Tests/TriangleTests.cs
@@ -1,12 +1,27 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TriangleTracker;
+using TriangleTracker.Models;
 
 namespace TriangleTracker.Tests {
   [TestClass]
   public class TriangleTrackerTests {
-    // [TestMethod]
-    // public void TriangleTest() {
 
-    // }
+  private Triangle _testTriangle;
+
+  public TriangleTrackerTests() {
+    _testTriangle = new Triangle(3, 4, 5);
+  }
+
+  [TestMethod]
+  public void Triangle_Side1HasExpectedValue_3() {
+    Assert.AreEqual(3, _testTriangle.Side1);
+  }
+  [TestMethod]
+  public void Triangle_Side2HasExpectedValue_4() {
+    Assert.AreEqual(4, _testTriangle.Side2);
+  }
+  [TestMethod]  
+  public void Triangle_Side3HasExpectedValue_5() {
+    Assert.AreEqual(5, _testTriangle.Side3);
+    }
   }
 }
